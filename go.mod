376b31724b5aeb102885cae81b72e331da424de2
Mohammad A. Apu <74357797+apuatcfbd@users.noreply.github.com>
2024-02-02 22:07:19 +0600

    Downloaded file from S3

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.26.6 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.16 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.15.15 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.3 // indirect
@@ -25,6 +26,7 @@ require (
 	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/bramvdbogaerde/go-scp v1.2.1 // indirect
 	github.com/fatih/color v1.16.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
--- internal/remotebackup/remote.go
+++ internal/remotebackup/remote.go
@@ -2,11 +2,17 @@ package remotebackup
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"log"
+	"os"
+	"path/filepath"
 )
+
+var bucketName = "all-server-backup-bucket"
 
 func ListBucket() {
 	// Load the Shared AWS Configuration (~/.aws/config)
@@ -18,14 +24,14 @@ func ListBucket() {
 		log.Fatal(err)
 	}
 
-	// todo: list bucket files
-
 	// Create an Amazon S3 service client
 	client := s3.NewFromConfig(cfg)
 
+	downloader := manager.NewDownloader(client)
+
 	// Get the first page of results for ListObjectsV2 for a bucket
 	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-		Bucket: aws.String("all-server-backup-bucket"),
+		Bucket: aws.String(bucketName),
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -35,4 +41,43 @@ func ListBucket() {
 	for _, object := range output.Contents {
 		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
 	}
+
+	//
+
+	dlFile := output.Contents[1].Key
+	resp, err := client.GetObject(context.Background(), &s3.GetObjectInput{
+		Bucket: &bucketName,
+		Key:    dlFile,
+	})
+	if err != nil {
+		fmt.Println("File download err for", dlFile, err.Error())
+	}
+	defer resp.Body.Close()
+
+	// todo: upload a file
+	dlErr := downloadToFile(downloader, "./downloads", bucketName, *dlFile)
+	if dlErr != nil {
+		fmt.Println("File download err", dlErr.Error())
+	}
+}
+
+func downloadToFile(downloader *manager.Downloader, targetDirectory, bucket, key string) error {
+	// Create the directories in the path
+	file := filepath.Join(targetDirectory, key)
+	if err := os.MkdirAll(filepath.Dir(file), 0775); err != nil {
+		return err
+	}
+
+	// Set up the local file
+	fd, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	// Download the file using the AWS SDK for Go
+	fmt.Printf("Downloading s3://%s/%s to %s...\n", bucket, key, file)
+	_, err = downloader.Download(context.TODO(), fd, &s3.GetObjectInput{Bucket: &bucket, Key: &key})
+
+	return err
 }
