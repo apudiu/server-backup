d5cfa00223af5d43ededb9f94deeccededac5fab
Mohammad A. Apu <74357797+apuatcfbd@users.noreply.github.com>
2024-02-08 17:00:05 +0600

    Removed previous impl of a fn

--- internal/tasks/task.go
+++ internal/tasks/task.go
@@ -28,48 +28,6 @@ func (t *Task) ExecuteLive(c *ssh.Client) (start, wait, closeFn func() error, er
 	return
 }
 
-//func (t *Task) ExecuteLive(c *ssh.Client) error {
-//	start, wait, closeFn, err := server.ExecCmdLive(c, t.Command[0], &t.StdOutErr)
-//	if err != nil {
-//		return err
-//	}
-//	defer func() {
-//		err = closeFn()
-//	}()
-//
-//	// read from task
-//	ch := make(chan struct{})
-//	l := logger.Logger{}
-//
-//	// read output while the cmd is executing
-//	go func() {
-//		l.ReadStream(&t.StdOutErr)
-//		ch <- struct{}{}
-//	}()
-//
-//	err = start()
-//	<-ch
-//
-//	err = wait()
-//	if err != nil {
-//		fmt.Println("Wait err", err.Error())
-//	}
-//
-//	l.WriteToFile("zip.log")
-//
-//	fmt.Println("after exec")
-//
-//	//for _, cmd := range t.Command {
-//	//	stdOut, stdErr, err := server.ExecCmd(c, cmd)
-//	//	if isEOFErr := errors.Is(err, io.EOF); !isEOFErr {
-//	//		return err
-//	//	}
-//	//
-//	//	t.Succeeded, t.StdOut, t.StdErr, t.ExecErr = true, stdOut, stdErr, err
-//	//}
-//	return nil
-//}
-
 func New(command string) *Task {
 	t := &Task{
 		Command: command,
